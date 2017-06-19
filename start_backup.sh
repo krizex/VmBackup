0924005a2e1b4bed8c23cca746aff7fd980c853c
Yang Qian <yang.qian@citrix.com>
2017-06-19 16:01:42 +0800

    add cronitor integration

--- start_backup.sh
+++ start_backup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 
+curl https://cronitor.link/5h4ISO/run -m 10 || true
+
 cd "$(dirname "$0")"
 
 python VmBackup.py xenroot CentOS7.cfg
 
+curl https://cronitor.link/5h4ISO/complete -m 10 || true
